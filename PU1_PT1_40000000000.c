6960befa0d0c15f444f02f08db99d9313826df5e
EdijsG <53660542+EdijsG@users.noreply.github.com>
2020-10-19 12:21:10 +0300

    2020.01.19

---
+++ PU1_PT1_40000000000.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+
+printf("Mainigo reizinasana: a x b\n");
+
+long int a;
+	printf("Pieskirt vertibu mainigajam - a :");
+	scanf("%ld",&a);
+	printf("Mainigais - a vertiba = %ld\n",a);
+long int b;
+        printf("Pieskirt vertibu mainigajam - b :");
+        scanf("%ld",&b);
+        printf("Mainigais - a vertiba = %ld\n",b);
+
+long int  c = a * b;
+
+	printf("%ld x %ld = %ld\n",a,b,c);
+}
+
