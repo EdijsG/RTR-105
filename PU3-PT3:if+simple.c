93efaf353f3e249d0c52b488b941330513758f63
EdijsG <53660542+EdijsG@users.noreply.github.com>
2020-11-01 23:54:30 +0200

    PU3/PT3: if + simple sort

---
+++ PU3-PT3:if+simple.c
@@ -0,0 +1,124 @@
+
+#include<stdio.h>
+
+
+int main()
+{
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf("			Dotos simblu sakartosana augosi vai dilstosi \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf("				Izveleties simbolu tipu \n");
+printf(" \n");
+printf(" \n");
+printf("-------------------- Ja velaties sakartot simbolus grupu 1. - latinu alfabeta no A lidz Z  ------------------ \n");
+printf(" \n");
+printf("-------------------------- Sadala: (Define simbolu grupu) ievieto : 1 --------------------------------------- \n");
+printf(" \n");
+printf("-------------------- Ja velaties sakartot simbolus grupu 2.- vesali skaitli no 0 lidz 9  -------------------- \n");
+printf(" \n");
+printf("-------------------------- Sadala: (Define simbolu grupu) ievieto : 2 --------------------------------------- \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf("               	Define simblu grupu (lai apstiprinatu izvaleto grupu nospiest - ENTER) : ");
+
+
+int sim_g = 0; // izveleta simblu grupa
+char simb_1 = 0;
+char simb_2 = 0;
+char simb_3 = 0;
+
+
+	scanf("%d",&sim_g);
+	
+	 if(sim_g == 1)
+	 {
+	printf(" \n");
+	printf(" \n");
+	printf(" \n");
+	printf(" \n");
+      	 printf("                                izveleta grupa - alfabets no A lidz W");
+	 }
+	 if(sim_g == 2 )
+	 {
+        printf(" \n");
+        printf(" \n");
+        printf(" \n");
+        printf(" \n");
+	 printf("                                izveleta grupa - skaitli no 0 lidz 9");
+	 }
+
+printf(" \n");
+printf(" \n");
+printf(" \n");
+printf("	Ievietojiet simbolus individuali atdalot tos (lai apstiprinatu izvaleto simbolu nospiestlai apstiprinatu\n	izvaleto simbolu nospiest - ENlai apstiprinatu izvaleto simbolu nospiest - EN - ENTER) \n");
+printf(" 			simbolu skaits ir limitets Max 3 individuali simboli\n");
+printf(" \n");
+printf(" \n");
+printf("	Definejiet simbolu Nr. 1\n	 (Lai apstiprinatu ievietoto simbolu nospiest - ENTER) :");
+	scanf("%c",simb_1);
+printf(" \n");
+printf(" \n");
+printf("        Definejiet simbolu Nr. 2\n 	(Lai apstiprinatu ievietoto simbolu nospiest - ENTER) :");
+        scanf("%c",simb_2);
+printf(" \n");
+printf(" \n");
+printf("        Definejiet simbolu Nr. 3\n       (Lai apstiprinatu ievietoto simbolu nospiest - ENTER) :");
+        scanf("%c",simb_3);
+printf(" \n");
+printf(" \n");
+printf("	Definetie simboli \n Nr. 1 %c\n Nr. 2 %c\n Nr. 3 %c\n");
+
+
+return 0;
+
+
+}
