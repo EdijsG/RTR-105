17df2dec3d92ab764dc11f9c7ed741c1623929be
EdijsG <53660542+EdijsG@users.noreply.github.com>
2020-11-19 00:56:10 +0200

    Update PU4-PT4:while+factorial.c

--- PU4-PT4:while+factorial.c
+++ PU4-PT4:while+factorial.c
@@ -1,8 +1,64 @@
+  GNU nano 4.8                                                                                                                                      PU4-PT4:while+factorial.c                                                                                                                                      Modified  
 #include<stdio.h>
 
-------- Kods vel ir tapsanas procesa. (Bet es isti nesmu izpratis prasibas uzdevumam.)
- :(
 int main(){
+        int n,i;
+        unsigned  long long fact =1
+        printf("Enter integer:");
+        scanf(%d, &n);
+
+        if(n<0){
+                printf("Error");
+        else(
+                for(i=1; i<=n; ++i){
+                        fact *=i;
+        )
+        }
+printf("Factorial of %d =%llu",n,fact);
+        }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+/*long char ch = 'll'; //Izvēlētais datu tips 
+
+int datt(){
+
+printf("\n\n\n\n\n\n\n\n\n");
+printf("        Lai izvēlētos datu tipu veidu norādiet to sadaļā | Izvēlētais datu tips | \n\n          Long long - ll\n\n              Char - c\n\n            Int - i\n\n     Izvēlētais datu tips:    ");
+scanf("%lc",&ch);
+/*      if(ch == 'll'){
+        printf("%c",ch);
+
+        }
+
+        if(ch == 2){ 
+        printf("%c",ch);
+
+        }
+
+        if(ch == 3){
+        printf("%c",ch);
+
+        }
+
+        else{
+
+        printf("|%lc| - Neeksistējoš datu tips",ch);
 
 return 0;
-{
+}
+                int main(){
+                        datt();
+                return 0;
+
+                }
+*/
+
