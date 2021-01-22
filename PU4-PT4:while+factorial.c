21bfaec2fe2eeac7fb5238d422831cf417a043f2
EdijsG <53660542+EdijsG@users.noreply.github.com>
2021-01-22 20:02:41 +0200

    Update PU4-PT4:while+factorial.c

--- PU4-PT4:while+factorial.c
+++ PU4-PT4:while+factorial.c
@@ -1,64 +1,80 @@
-  GNU nano 4.8                                                                                                                                      PU4-PT4:while+factorial.c                                                                                                                                      Modified  
+
 #include<stdio.h>
-
 int main(){
-        int n,i;
-        unsigned  long long fact =1
-        printf("Enter integer:");
-        scanf(%d, &n);
-
-        if(n<0){
-                printf("Error");
-        else(
-                for(i=1; i<=n; ++i){
-                        fact *=i;
-        )
-        }
-printf("Factorial of %d =%llu",n,fact);
-        }
-return 0;
-}
-
-
-
-
-
-
-
-
-
-/*long char ch = 'll'; //Izvēlētais datu tips 
-
-int datt(){
-
-printf("\n\n\n\n\n\n\n\n\n");
-printf("        Lai izvēlētos datu tipu veidu norādiet to sadaļā | Izvēlētais datu tips | \n\n          Long long - ll\n\n              Char - c\n\n            Int - i\n\n     Izvēlētais datu tips:    ");
-scanf("%lc",&ch);
-/*      if(ch == 'll'){
-        printf("%c",ch);
-
-        }
-
-        if(ch == 2){ 
-        printf("%c",ch);
-
-        }
-
-        if(ch == 3){
-        printf("%c",ch);
-
-        }
-
+    int skaitlis;
+    int i=1;
+    long long b=1;
+    long long c=1;
+    int intNum=1;
+    char charNum=1;
+    long long longNum=1;
+    char izvele;
+  
+    printf("Ievadiet decimālu skaitli: ");
+    scanf("%d",&skaitlis);
+    printf("Izvēlieties vienu no dotajiem datu tipiem: char(c), int(i), long long(l): ");
+    scanf(" %c", &izvele);
+    while(i<=skaitlis){
+        b=b*i;
+        i++;
+      }
+  
+    i = 1;
+  
+    if(izvele == 'i'){
+        while(i<=skaitlis){
+            intNum = intNum * i;
+            c = c * i;
+            i++;
+            if (intNum != c){
+                break;
+              }
+            }
+      
+        if((long long)intNum != b){
+                printf("Ar šo datu tipu nav iespējams aprēķināt jūsu ievadīto skaitli");
+            } 
         else{
-
-        printf("|%lc| - Neeksistējoš datu tips",ch);
-
-return 0;
+          printf("Faktoriāls no skaitļa %d ir %d\n", skaitlis, intNum);
+            }    
+        }
+  
+  else if(izvele == 'c'){
+     while(i<=skaitlis){
+         charNum = charNum * i;
+         c=c*i;
+          i++;
+        if(charNum != c){
+           break;
+          }
+        }
+    
+      if((long long)charNum != b){
+        printf("Ar šo datu tipu nav iespējams aprēķināt jūsu ievadīto skaitli");
+         }
+    
+      else{
+         printf("Faktoriāls no skaitļa %d ir %d\n", skaitlis, charNum);
+         }
+    }
+  
+  else if(izvele == 'l'){
+      while(i<=skaitlis){
+          longNum = longNum * i;
+          c=c*i;
+            i++;
+            if (longNum != c)
+            {
+                break;
+            }
+        }
+            if(i>21)
+            {
+                printf("Ar šo datu tipu nav iespējams aprēķināt jūsu ievadīto skaitli");
+            }
+            else
+            {
+                printf("Faktoriāls no skaitļa %d ir %lld\n", skaitlis, longNum);
+            }
+    }
 }
-                int main(){
-                        datt();
-                return 0;
-
-                }
-*/
-
