b071f7c05bb5b1c20cde89c338c951b8b27c2e77
EdijsG <53660542+EdijsG@users.noreply.github.com>
2021-01-22 20:11:59 +0200

    Update PU5-PT5:for+factorial.c

--- PU5-PT5:for+factorial.c
+++ PU5-PT5:for+factorial.c
@@ -1,9 +1,69 @@
 #include<stdio.h>
 
-------- Kods vel ir tapsanas procesa. (Bet es isti nesmu izpratis prasibas uzdevumam.)
- :(
+ int i,fact=1,number,intNum=1;
+ long long b=1,longNum=1;
+ char izvele, charNum=1;
+
 
 int main(){
-
-return 0;
-}
+ printf("Izvēlieties datu tipu: char(c), int(i), long long(l):");
+ scanf("%c",&izvele);
+ printf("/nIevadiet decimālu skaitli:");    
+ scanf("%d",&number);
+ 
+  for(i=1;i<=number;i++){    
+    fact=fact*i;    
+     }
+ 
+  if(izvele == 'i'){
+    for(i=1;i<=number;i++){
+      intNum=intNum*i;
+      b=b*i;
+        if(intNum != b){
+         break;
+          }
+       }
+   
+    if((long long)intNum != b){
+      printf("Ar šo datu tipu nevar aprēķināt faktoriālu");
+      }
+    else{
+      printf("Faktoriāls no %d is: %d\n",number,intNum);
+      }
+  }
+  else if (izvele == 'c'){
+    for(i=1;i<=number;i++)
+    {
+  charNum=charNum*i;
+  b=b*i;
+    if(charNum != b){
+        break;
+      }
+    }
+   
+    if((long long)charNum != b){
+      printf("Ar šo datu tipu nevar aprēķināt faktoriālu");
+      }
+    else {
+      printf("Faktoriāls no %d is: %d\n",number,charNum);
+      }
+    }
+  else if (izvele == 'l'){
+    for(i=1;i<=number;i++){
+      longNum=longNum*i;
+      b=b*i;
+      if (longNum != b){
+        break;
+        }
+      }
+   
+    if(i>21){
+      printf("Ar izvēlēto datu tipu nevar irzēķinārt datu tipu");
+      }
+   
+    else{
+       printf("Faktoriāls no %d is: %lld\n",number,longNum);
+      }
+  }
+return 0;  
+}  
